9649d7ccc1187b2e5dfac36b4ccdec3162b74a73
Oskar Polak <oskarpolak@vp.pl>
2017-11-29 15:46:05 +0100

    df

--- src/Time.java
+++ src/Time.java
@@ -1,4 +1,5 @@
 import java.time.*;
+import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.time.temporal.TemporalUnit;
 
@@ -9,5 +10,12 @@ public class Time {
 
         System.out.println(
                 Duration.between(localDateTime, LocalDateTime.now()).toHours());
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        LocalDateTime date = LocalDateTime.parse("1921-11-11 13:40:20", formatter);
+        LocalDateTime now = LocalDateTime.now();
+
+        DateTimeFormatter myFormatter = DateTimeFormatter.ofPattern("HH:mm");
+        System.out.println(myFormatter.format(now));
     }
 }
